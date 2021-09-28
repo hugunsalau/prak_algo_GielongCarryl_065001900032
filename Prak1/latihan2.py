c87a48645c24d47f6f36b2e3a442e273810c76fd
hugunsalau <91172333+hugunsalau@users.noreply.github.com>
2021-09-28 21:53:14 +0700

    upload latihan2

--- Prak1/latihan2.py
+++ Prak1/latihan2.py
@@ -1,3 +1,13 @@
-# capitalization to generate camel case
-name1 = input("masukkan kata :")
-print(name1.upper())
+import math
+
+a=input("Masukkan angka pertama : ")
+
+p1 = a.split(",")
+
+b=input("Masukkan angka kedua : ")
+
+p2 = b.split(",")
+
+jarak = math.sqrt( ((int(p1[0])-int(p2[0]))**2)+((int(p1[1])-int(p2[1]))**2) )
+
+print("Jarak antara ", a,"dan", b, "adalah",jarak) 
